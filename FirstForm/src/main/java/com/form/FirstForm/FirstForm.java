0d62f1a070420a700dc84c05aefea4383e41a32e
Diana <Diana@dya>
2016-07-04 14:46:29 +0300

    Form

--- FirstForm/src/main/java/com/form/FirstForm/FirstForm.java
+++ FirstForm/src/main/java/com/form/FirstForm/FirstForm.java
@@ -1,9 +1,12 @@
-package com.form.servlets.FirstForm;
+package com.form.FirstForm;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,8 +14,18 @@ import javax.servlet.http.HttpServletResponse;
 public class FirstForm extends HttpServlet{
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		PrintWriter out=resp.getWriter();
+		
 		resp.setContentType("text/html");
 		out.println("Hello World");
+		
+		
+		
+
+		RequestDispatcher rd = req.getRequestDispatcher("form.html");
+		rd.include(req, resp);
+		((HttpServletResponse) resp).sendRedirect("form.html");
+		
+		
 		out.close();
 	}
 }
